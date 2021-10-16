83f65891e463c4a528e6b5c204161b8dd559c516
nicopimienta <nicolaspimienta@hotmail.com>
2021-10-16 00:21:45 -0300

    actualizado app

--- js/app.js
+++ js/app.js
@@ -82,8 +82,13 @@
 // 
 // indiceCorporal()
 
-// blablaalbal
 
+
+
+
+// ====== TP COMPLEMENTARIO CLASE 04 ======
+
+// EJERCICIO 1
 
 // const precioCosto = () => {
 //     let precio = parseInt(prompt('Ingrese el precio'))
@@ -97,11 +102,120 @@
 
 // sumarIva()
 
-// 
+// EJERCICIO 2
 
-const recibir = (numeroDec) => {
-    let traerdecimal = parseFloat(prompt('Ingrese numero decimal'))
-    return alert(Math.round(traerdecimal))
+// let edad = prompt("Cual es tu edad?");
 
+
+
+// const checkEdad = () => {
+//     if (Number(edad) < 18) {
+//         alert("lo lamento, sos muy joven para subirte sacar la licencia");
+//     } else if (Number(edad) > 18) {
+//         alert("ARRANCAMOS!!!, a ponerse el cinturón! ");
+//     } else if (Number(edad) === 18) {
+//         alert("Felicidades en tu primer año de conducción!, a ponerse el cinturón")
+//     }
+
+// }
+
+// checkEdad()
+
+
+// EJERCICIO 3
+
+// let ivaImpuesto = 0.21
+// let impuestoPais = 0.08
+// let impuestoGanancias = 0.35
+// let pedirNombreDeJuego = prompt('Ingrese el nombre del juego')
+// let juegoSteam = parseFloat((prompt('Ingrese el valor del juego')))
+
+// const sumaDeImpuestos = () => {
+//     let suma = (ivaImpuesto + impuestoPais + impuestoGanancias) * 100
+//     let porcentajeMasValor = juegoSteam + (suma / 100 * juegoSteam)
+//     return porcentajeMasValor
+// }
+
+// const valorNeto = () => {
+//     console.log('El valor total de ' + pedirNombreDeJuego + ' más impuestos  es de ' + sumaDeImpuestos());
+// }
+
+
+// valorNeto()
+
+
+// ====== Clase 05 -  Objetos =========
+
+
+
+
+class PesoRef {
+    constructor (condicion,actividadRecom,intensidad) {
+            this.condicion = condicion
+            this.actividadRecom = actividadRecom
+            this.intensidad = intensidad
 }
+    // Metodo
+    preguntarNombre() {
+        let nombre = prompt('Ingrese su nombre')
+        return nombre
+    }
+
+    }
+
+// Los valores de intensidad son aproximados y varian dependiendo la persona (Pienso aclararlo en algun lugar de la aplicación)
+
+
+
+const pesoRef1 = new PesoRef ("Bajo de peso", "Entrenamiento Personalizado", "Media")
+const pesoRef2 = new PesoRef ("Normal/Ideal", "Entrenamiento Funcional","Media-Alta")
+const pesoRef3 = new PesoRef ("Sobre peso", "actividades aerobicas y de fuerza","Alta")
+const pesoRef4 = new PesoRef ("Obesidad","Spinning o Entrenamiento Personalizado","Media-Alta" )
+
+
+
+
+
+const indiceCorporal = () => {
+    let peso =  parseInt(prompt('Ingrese su peso'))
+    let altura = parseInt(prompt('Ingrese su altura'))
+    let resultado = Math.round( peso / (altura * altura)*10000)
+    alert('hola' + ' ' + pesoRef1.preguntarNombre() + ' su IMC es de' + " " + resultado)
+    nivelPeso(resultado)
+}
+
+
+
+
+const nivelPeso = (resultado) => {
+    if (resultado <= 18){
+    console.log('Estás ' + pesoRef1.condicion);
+    console.log(`Nuestra recomendación de actividad : ${pesoRef1.actividadRecom} , que tiene intensidad: ${pesoRef1.intensidad} `);
+    } else if ((resultado > 18) && (resultado <= 25)){
+        console.log('Estás en un peso ' + pesoRef2.condicion);
+        console.log(`Nuestra recomendación de actividad : ${pesoRef2.actividadRecom} , que tiene intensidad: ${pesoRef2.intensidad} `);
+    } else if ((resultado > 25) && (resultado <= 29)) {
+        console.log('Estás con ' + pesoRef3.condicion);
+        console.log(`Nuestra recomendación de actividad : ${pesoRef3.actividadRecom} , que tiene intensidad: ${pesoRef3.intensidad} `);
+    } else if (resultado >= 30) {
+        console.log('Estás con ' + pesoRef4.condicion);
+        console.log(`Nuestra recomendación de actividad : ${pesoRef4.actividadRecom} , que tiene intensidad: ${pesoRef4.intensidad} `);
+    } else {
+        console.log('error');
+    }
+}
+
+indiceCorporal()
+
+
+
+
+// agregar a futuro un switch para elección de profesores
+
+
+
+
+
+
+
 
