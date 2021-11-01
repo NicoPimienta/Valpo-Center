4a6205e4331c235d37bbf70e9bb867df8a646fe2
nicopimienta <nicolaspimienta@hotmail.com>
2021-11-01 17:34:44 -0300

    eventos incluidos

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// ====== Entrega de Proyecto final 1° =========
+// ====== Entrega de Proyecto final 2° =========
 
 
 
@@ -8,17 +8,18 @@
 
 class PesoRef {
     constructor (condicion,actividadRecom,intensidad) {
-            this.condicion = condicion
-            this.actividadRecom = actividadRecom
-            this.intensidad = intensidad
-}
+        this.condicion = condicion
+        this.actividadRecom = actividadRecom
+        this.intensidad = intensidad
+    }
     // Metodo
     preguntarNombre() {
         let nombre = prompt('Ingrese su nombre')
+        
         return nombre
     }
-
-    }
+    
+}
 
 // Los valores de intensidad son aproximados y varian dependiendo la persona (Pienso aclararlo en algun lugar de la aplicación)
 
@@ -28,6 +29,10 @@ new PesoRef ("Normal/Ideal", "Entrenamiento Funcional","Media-Alta"),
 new PesoRef ("Sobre peso", "actividades aerobicas y de fuerza","Alta"),
 new PesoRef ("Obesidad","Spinning o Entrenamiento Personalizado","Media-Alta"),
 ]
+localStorage.setItem('pesoActi', JSON.stringify(pesoActi))
+
+
+
 
 const profesores = ['Victoria Amoroso', 'Geronimo Martin' , 'Luna Gancedo', 'Jonatan Demichelis']
 
@@ -53,130 +58,161 @@ const staff = profesores.concat(Kinesiologos)
 
 
 
-// ======== Función principal ========
-
-
-const indiceCorporal = () => {
-    let peso =  parseInt(prompt('Ingrese su peso'))
-    let altura = parseInt(prompt('Ingrese su altura'))
-    let resultado = Math.round( peso / (altura * altura)*10000)
-    alert('hola' + ' ' + pesoActi[0].preguntarNombre() + ' su IMC es de' + " " + resultado)
-    alert(`hola ${pesoActi[0].preguntarNombre()} `)
-    nivelPeso(resultado)
-}
 
 
 
 
-const nivelPeso = (resultado) => {
+
+
+
+
+
+
+// =============== Estructura principal de la app. ============
+
+// ============ DOM ===============
+
+const contenedor = document.getElementById('contenedor')
+contenedor.classList.add('container')
+
+const div = document.createElement('div')
+div.classList.add('row')
+contenedor.append(div)
+
+const formulario = document.createElement('form')
+formulario.classList.add('card', 'col-12', 'bg-warning' , 'p-5')
+formulario.setAttribute("id", "formu");
+formulario.innerHTML = `
+<h1 class="display-4 text-center">Conocé tu masa corporal</h1>
+<p class="text-muted fs-2 text-center">El índice de masa corporal (IMC) es una razón matemática que asocia la masa y la talla de un individuo, ideada por el estadístico belga Adolphe Quetelet, por lo que también se conoce como índice de Quetelet. </p>
+<div class="input-group mb-5">
+<span class="input-group-text" id="inputGroup-sizing-default">Nombre</span>
+<input type="text" id="inputNombre" value= "" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+</div>
+
+<div class="input-group  mb-5 ">
+<span class="input-group-text" id="inputGroup-sizing-default">Peso</span>
+<input type="number" id="inputPeso" value= "" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+</div>
+
+<div class="input-group mb-5">
+<span class="input-group-text" id="inputGroup-sizing-default">Altura</span>
+<input type="number" id="inputAltura" value= "" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+</div>
+
+`
+
+contenedor.append(formulario)
+
+
+// Botón
+
+const boton = document.createElement('button')
+boton.classList.add('btn', 'btn-secondary' , 'mb-5')
+boton.innerHTML = ` <button  type="button" id="boton"><b>Ver Resultado</b></button> `
+
+
+formulario.append(boton)
+
+
+
+// Display donde se muestra el resultado
+
+const divResultado = document.createElement('div')
+divResultado.classList.add('card', 'col-12', 'bg-dark' , 'p-5')
+// divResultado.innerHTML = `<i class="bi bi-info" data-bs-toggle="tooltip" data-bs-html="true" title="Los valores de intensidad son aproximados y varian dependiendo la persona" style="font-size: xx-large"></i>`
+formulario.append(divResultado)
+
+
+const botonReset = document.createElement('button')
+botonReset.classList.add('btn', 'btn-secondary' , 'p-2')
+botonReset.setAttribute("id", "Resetear");
+botonReset.innerText = `Reset`
+
+formulario.append(botonReset)
+
+
+
+
+//  ============EVENTO===========
+
+const botonEvento =  document.getElementById('boton')
+const botonDeReset = document.getElementById('Resetear')
+
+    
+    
+    // ===== Función para evento =====
+    
+    
+    
+    const formuEvento = document.getElementById('formu')
+    
+    formuEvento.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    const valorNombre = getNombre.value
+    const valorPeso = parseInt(getPeso.value)
+    const valorAltura = parseInt(getAltura.value)
+    let resultado = Math.round( valorPeso / (valorAltura * valorAltura)*10000)
+    
+    localStorage.setItem('usuario', JSON.stringify(valorNombre))
+    localStorage.setItem('valorPeso', JSON.stringify(valorPeso))
+    localStorage.setItem('valorAltura', JSON.stringify(valorAltura))
+    nivelPeso(resultado , valorNombre)
+    
+} )
+
+
+// Función complementaria
+
+const nivelPeso = (resultado , valorNombre) => {
     if (resultado <= 18){
-    console.log('Estás ' + pesoActi[0].condicion);
-    console.log(`Nuestra recomendación de actividad : ${pesoActi[0].actividadRecom} , que tiene intensidad: ${pesoActi[0].intensidad} `);
+        divResultado.innerHTML = `Hola ${valorNombre}  su IMC es de  ${resultado}.
+        <b>Estás con ${pesoActi[0].condicion}</b> <b>Nuestra recomendación de actividad : ${pesoActi[0].actividadRecom} , que tiene intensidad: ${pesoActi[0].intensidad}</b> `;
     } else if ((resultado > 18) && (resultado <= 25)){
-        console.log('Estás en un peso ' + pesoActi[1].condicion);
-        console.log(`Nuestra recomendación de actividad : ${pesoActi[1].actividadRecom} , que tiene intensidad: ${pesoActi[1].intensidad} `);
+        divResultado.innerHTML = `<b>Hola ${valorNombre}  su IMC es de  ${resultado}.
+        <b>Estás en un peso ${pesoActi[1].condicion}</b> <b>Nuestra recomendación de actividad : ${pesoActi[1].actividadRecom} , que tiene intensidad: ${pesoActi[1].intensidad}</b>`
     } else if ((resultado > 25) && (resultado <= 29)) {
-        console.log('Estás con ' + pesoActi[2].condicion);
-        console.log(`Nuestra recomendación de actividad : ${pesoActi[2].actividadRecom} , que tiene intensidad: ${pesoActi[2].intensidad} `);
+        divResultado.innerHTML =`<b>Hola ${valorNombre}  su IMC es de  ${resultado}.
+        <b>Estás con ${pesoActi[2].condicion}</b> .<b>Nuestra recomendación de actividad : ${pesoActi[2].actividadRecom} , que tiene intensidad: ${pesoActi[2].intensidad}</b> `
     } else if (resultado >= 30) {
-        console.log('Estás con ' + pesoActi[3].condicion);
-        console.log(`Nuestra recomendación de actividad : ${pesoActi[3].actividadRecom} , que tiene intensidad: ${pesoActi[3].intensidad} `);
+        divResultado.innerHTML = `<b>Hola ${valorNombre}  su IMC es de  ${resultado}.
+        <b>Estás con  ${pesoActi[3].condicion}</b>. <b>Nuestra recomendación de actividad : ${pesoActi[3].actividadRecom} , que tiene intensidad: ${pesoActi[3].intensidad}</b>`
     } else {
-        console.log('error');
+        divResultado.innerHTML = ``;
     }
 }
 
-// indiceCorporal()
 
-// ^^ descomentar para  probar
+// Reset de los inpunts
+let inputsTodos = document.querySelectorAll('input')
 
+botonDeReset.addEventListener('click', () => {
+    inputsTodos.forEach(input => input.value = '')
 
-// agregar a futuro un switch para elección de profesores x horario.
-
-
-
-
+})
 
 
 
-// ============ DOM ===============
-// ABAAJO ENTREGABLE (poner en comentario despues de los "=")
 
-const cuerpoApp = document.getElementById ('cuerpo')
-const contenedor = document.getElementsByClassName('contenedor')
+// ==================
 
-cuerpoApp.innerHTML = `<h1><strong>Indice de masa corporal</strong></h1>`
-cuerpoApp.classList.add("fondo")
+botonEvento.addEventListener('click', formuEvento)
 
 
-document.body.appendChild(cuerpoApp);
+const getNombre = document.getElementById('inputNombre')
+const getPeso = document.getElementById('inputPeso')
+const getAltura = document.getElementById('inputAltura')
 
 
 
-let printNombre = prompt('Ingrese su nombre')
-
-const pedirInfo1 = document.createElement('p')
-
-pedirInfo1.innerHTML = (`<i>Hola ${printNombre}!! , nuestro staff se conforma por
-
-${profesores.join(" , ")} , ${Kinesiologos.join(" , ")} </i> `)
 
 
-pedirInfo1.classList.add("fondo")
-
-document.body.append(pedirInfo1)
-
-
-
-// ========================================
-
-
-// PASARLO A BOOTSTRAP
-
-
-// 1.Crear el contenedor- x
-// 2.Crear los textos - x
-// 3. Crear los inputs (usar los ranges de BS) -------- Agregar formu común, agregar Range cuando se vea jQuery
-// 4. Agregar un botón de enviar (Via BS) - x
-// 5. Darle color
 // PD: Ver ''progress'' de BS y ver como implementarlo. (https://getbootstrap.com/docs/5.0/components/progress/)
-
-// const contenedor = document.querySelector('.container .row')
-
 // REEMPLAZAR INPUTS DE TEXTOS POR RANGE'S
 
-
-// const formulario = document.createElement('article')
-//     formulario.classList.add('card', 'col-12', 'bg-warning')
-//     formulario.innerHTML= `
-//     <h1 class="display-4 text-center">Conocé tu masa corporal</h1>
-//     <p class="text-muted fs-2 text-center">El índice de masa corporal (IMC) es una razón matemática que asocia la masa y la talla de un individuo, ideada por el estadístico belga Adolphe Quetelet, por lo que también se conoce como índice de Quetelet. </p>
-//     <div class="input-group mb-5">
-//     <span class="input-group-text" id="inputGroup-sizing-default">Nombre</span>
-//     <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-//     </div>
-    
-//     <div class="input-group mb-5 ">
-//     <span class="input-group-text" id="inputGroup-sizing-default">Peso</span>
-//     <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-//     </div>
-
-//     <div class="input-group mb-5">
-//     <span class="input-group-text" id="inputGroup-sizing-default">Altura</span>
-//     <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-//     </div>
-
-//     <button type="button" class="btn btn-secondary mb-5"><b>Ver Resultado</b></button>
-
-//     `
+// agregar tipo modal de clase actividades en resultado
 
 
-// contenedor.append(formulario)
+//  Crear los inputs (usar los ranges de BS) -------- Agregar formu común, agregar Range cuando se vea jQuery
 
-
-
-
-
-
-
-// console.log(formulario);
+//Crear un modal que pida e-mail.
